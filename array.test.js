1bb3090d0a607451649ba76bde9d9460965d373d
Adryano Junior <jusamsung10@gmail.com>
2024-11-16 20:16:26 -0300

    change array.test

--- array.test.js
+++ array.test.js
@@ -4,7 +4,7 @@ describe('Função encontrar Indices', () => {
 
     it('Em um array númerico, deve retornar os índices do maior e menor valor', () => {
         const array = [10, 20, 5, 8];
-        const resultado = encontrarIndices(array);
+        const resultado = foundIndices(array);
         expect(resultado).toEqual({ indiceMaior: 1, indiceMenor: 2 });
     });
     });
